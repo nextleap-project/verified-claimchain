5767cf00535b1ac5ce359aafd0b03354190c2862
Anilam <malina.wein@gmail.com>
2017-02-09 12:46:28 +0100

    Upd

--- SkipList/C/SkipList.c
+++ SkipList/C/SkipList.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,8 +5,9 @@
 
 struct Leaf { 
     int key; 
-    int **pointers;
     int level;
+    int* pointers;
+
 };
 
 typedef struct Leaf Leaf;
@@ -15,33 +15,83 @@ typedef struct Leaf Leaf;
 
 void printStructure(Leaf l)
 {
-    printf("%d\n", l.key);
+    int i=0;
+    printf("key: %d\n", l.key);
+    printf("level: %d\n", l.level);
+    for(i = 0; i<l.level; i++)
+    {
+        printf("pointer: %p\n", l.pointers[i]);
+    }
 }
 
-int random_level(int max_level)
+int random_level( int max_level)
 {
-    srand(time(NULL));
-    return rand()%max_level;
+    return (rand()%max_level+1);
 }
 
 void generateOneLeaf(Leaf * a, int key, int max_level)
 {    
-    (*a).key = 10;
-    (*a).level = random_level(max_level);
+    int counter = 0;
+    (*a).key = key;
+    int level = random_level(max_level);
+    (*a).level = level;
+    (*a).pointers = (int*)malloc(sizeof(int*)*level);
 }
 
+void buildReference(Leaf* array, int size)
+{
+    int i = 0;
+    int j = 0; 
+    int k = 0;
+
+    printf("%d\n", (*array).key);
+    /*for (i = 0; i < size; i++)
+    {
+        for (j = 0; j<(*array[i]).level; j++)
+        {
+            for (k  = i+1; k<size; k++ )
+            {
+                if (array[k].level < j)
+                {
+                    array[i].pointers[j] = (array[k]);
+                }
+            }
+
+            if (array[i].pointers[j] = 0)
+            {
+                array[i].pointers[j] = array[size];
+            }
+        }
+    }*/
+}
 int main ()
 {
+    srand(time(NULL));
     int size = 7;
     int i= 0;
-    int max_level = (int)ceil(log(size));
-    Leaf a[size];
-    for (i = 0; i<size; i++)
-    {
-        generateOneLeaf(&a[i], 1, max_level);
+    int array[size];
+    int max_level = (int)ceil(log2(size));
+
+    array[0]=3;
+    array[1]=5;
+    array[2]=6;
+    array[3]=20;
+    array[4]=21;
+    array[5]=25;
+    array[6]=26;
+
+    Leaf array_to_sort[size+1];
+    for (i = 0; i<size; i++){
+        generateOneLeaf(&array_to_sort[i], array[i], max_level);
+        printStructure(array_to_sort[i]);
     }
 
-    printStructure(a[0]);
+
+    Leaf last;
+    last.key = 10;
+    last.level = max_level;
+    array_to_sort[size] = last;
+
     return 0;
 
 }
