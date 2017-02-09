8c3b6abcb7ec2bb4b046f568a7246c7384b5256d
Anilam <malina.wein@gmail.com>
2017-02-09 16:25:02 +0100

    Update

--- SkipList/C/SkipList.c
+++ SkipList/C/SkipList.c
@@ -3,18 +3,20 @@
 #include <time.h>
 #include <math.h>
 
+
+
 struct LL
 {
-struct Leaf * leaf;
-struct LL * pointer;
+    struct Leaf * leaf;
+    struct LL * pointer;
 };
 
 typedef struct LL LL;
 
 struct Leaf { 
-int key; 
-int level;
-LL* pointers;
+    int key; 
+    int level;
+    LL* pointers;
 
 };
 
@@ -22,53 +24,55 @@ typedef struct Leaf Leaf;
 
 int random_level( int max_level)
 {
-return (rand()%max_level+1);
+    return (rand()%max_level+1);
 }
 
 Leaf* generateOneLeaf(int key, int max_level)
-{ 
-int levels = random_level(max_level);
-LL* pointers = (LL*)malloc(sizeof(LL)*levels);
+{    
+    int levels = random_level(max_level);
+    LL* pointers = (LL*)malloc(sizeof(LL)*levels);
 
-Leaf* leaf = (Leaf*)(malloc(sizeof(Leaf)));
-(*leaf).key = key;
-(*leaf).level = levels;
-(*leaf).pointers = pointers;
+    Leaf* leaf = (Leaf*)(malloc(sizeof(Leaf)));
+    leaf->key = key;
+    leaf->level = levels;
+    leaf->pointers = pointers;
 
-return leaf;
+    return leaf;
 }
+
 
 int main ()
 {
-srand(time(NULL));
-int size = 7;
-int i= 0;
-int array[size];
-int max_level = (int)ceil(log2(size));
+    srand(time(NULL));
+    int size = 7;
+    int i= 0;
+    int array[size];
+    int max_level = (int)ceil(log2(size));
 
-array[0]=3;
-array[1]=5;
-array[2]=6;
-array[3]=20;
-array[4]=21;
-array[5]=25;
-array[6]=26;
+    array[0]=3;
+    array[1]=5;
+    array[2]=6;
+    array[3]=20;
+    array[4]=21;
+    array[5]=25;
+    array[6]=26;
 
-LL* llheap = (LL *)malloc(sizeof (LL)*max_level);
-Leaf* heap = (Leaf*)malloc(sizeof (Leaf));
-heap->key = 0;
-heap->level = max_level;
-heap->pointers = llheap;
+    LL* llheap = (LL *)malloc(sizeof (LL)*max_level);
+    Leaf* heap = (Leaf*)malloc(sizeof (Leaf));
+    heap->key = 0;
+    heap->level = max_level;
+    heap->pointers = llheap;
 
-Leaf* current = heap;
+    Leaf* current = heap;
 
-for (i =0; i<size; i++)
-{
-Leaf* newLeaf= generateOneLeaf(array[i],max_level);
-((current->pointers)[0]).leaf = newLeaf;
-current = newLeaf;
-}
+    for (i =0; i<size; i++)
+    {
+        Leaf* newLeaf= generateOneLeaf(array[i],max_level);
+        ((current->pointers)[0]).leaf = newLeaf;
+        current = newLeaf;
+    }
 
-return 0;
+
+    return 0;
 
 }
